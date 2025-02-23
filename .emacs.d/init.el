052549abbe4a5b7a4d586932fa89fcd824c14e14
Jordan Collocott <jrdn.collocott@gmail.com>
2025-02-23 12:43:19 +0200

    config(emacs): init

---
+++ .emacs.d/init.el
@@ -0,0 +1,102 @@
+;; Store custom-set-variables in a separate file
+(setq custom-file "~/.emacs.d/emacs_custom.el")
+
+;; Disable some GUI stuff
+(setq inhibit-startup-message t)
+(menu-bar-mode 0)
+(scroll-bar-mode 0)
+(tool-bar-mode 0)
+(tooltip-mode 0)
+
+;; Disable annoying bell
+(setq ring-bell-function 'ignore)
+;; Disable the scroll bars when splitting screen
+(add-to-list 'default-frame-alist
+	     '(vertical-scroll-bars . nil))
+
+;; Set tab width to 4
+(setq c-default-style "linux")
+(setq-default indent-tabs-mode nil
+			  c-basic-offset 4
+			  tab-width 4)
+
+(load-theme 'tango-dark t)
+
+;; Don't freeze emacs with ctrl-z
+(global-unset-key (kbd "C-z"))
+
+;; Don't create backup files
+(setq backup-inhibited t)
+(setq auto-save-default nil)
+
+;; View column number of cursor position
+(setq column-number-mode t)
+
+;; Auto-insert pairs
+;; (electric-pair-mode 1)
+
+;; Line numbers
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+(dolist (mode '(org-mode-hook
+		vterm-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0)))) ;; Disable line numbers in some buffers
+
+;; tree-sitter
+(setq treesit-language-source-alist
+      '((c "https://github.com/tree-sitter/tree-sitter-c")
+        (cmake "https://github.com/uyha/tree-sitter-cmake")
+        (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+        (python "https://github.com/tree-sitter/tree-sitter-python")
+        (json "https://github.com/tree-sitter/tree-sitter-json")))
+;; mask major modes to use treesitter instead
+(setq major-mode-remap-alist
+      '((c-mode . c-ts-mode)
+        (python-mode . python-ts-mode)
+        (json-mode . json-ts-mode)))
+
+;; Package management
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t) ;; If not installed, install them automatically
+;; Install the diminish package to not show the modes for each active package on the modeline
+(use-package diminish)
+
+;; For managing git repositories
+(use-package magit
+  ;;:custom 
+  ;;(magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1) ; Don't open new window when viewing diff
+  )
+
+;; Better completion
+(use-package ivy
+  :diminish ivy-mode
+  :config
+  (ivy-mode 1))
+
+;; Full shell
+(use-package vterm
+  :ensure t)
+
+;; Show available key-chords
+(use-package which-key
+  :init (which-key-mode) ; init: runs before the package is loaded
+  :diminish which-key-mode
+  :config ; config: runs after the package is loaded
+  (setq which-key-idle-delay 0.5))
+
+;; Org-mode
+(use-package org)
+
+;; Load the custom-set-variables file
+(load-file custom-file)
