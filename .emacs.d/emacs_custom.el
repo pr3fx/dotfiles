932b8af800eb36a96010f214c018bab27f9f265a
Jordan Collocott <jrdn.collocott@gmail.com>
2026-01-25 07:50:46 +0200

    config(emacs): clean up config file structure, add whitespace mode

--- .emacs.d/emacs_custom.el
+++ .emacs.d/emacs_custom.el
@@ -5,8 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("a76410c06e29fa659577ce87e99fc193cd6975961357e3df914dde0f6b6d8746"
-     default))
- '(package-selected-packages '(which-key magit diminish)))
+	 default))
+ '(package-selected-packages
+   '(## beacon diminish dired-x ido-completing-read+ magit
+		nord-theme projectile smex )))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,12 +1,34 @@
 ;; Store custom-set-variables in a separate file
 (setq custom-file "~/.emacs.d/emacs_custom.el")
 
-;; Disable some GUI stuff
+;; ------------------
+;; Setup use-package
+;; ------------------
+;; Package management
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https://orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+;; Initialize use-package
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t) ;; If not installed, install them automatically
+
+;; ------------------
+;; GUI configurations
+;; ------------------
 (setq inhibit-startup-message t)
 (menu-bar-mode 0)
 (scroll-bar-mode 0)
 (tool-bar-mode 0)
 (tooltip-mode 0)
+(setq column-number-mode t) ;; view column number of cursor position
+(show-paren-mode 1) ;; highlight matching parenthesis
 
 ;; Set font
 (set-face-attribute 'default nil
@@ -15,14 +37,10 @@
                     :weight 'normal
                     :width 'normal)
 
-;; Inline code completion
-;;(global-completion-preview-mode)
-
-;; Disable annoying bell
-(setq ring-bell-function 'ignore)
 ;; Disable the scroll bars when splitting screen
 (add-to-list 'default-frame-alist
-	         '(vertical-scroll-bars . nil))
+             '(vertical-scroll-bars . nil))
+
 
 ;; disable cursor blinking
 (setq blink-cursor-mode nil)
@@ -31,22 +49,63 @@
 (setq-default truncate-lines t)
 (setq-default truncate-partial-width-windows nil)
 
+;; Line numbers
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode)
+(dolist (mode '(org-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0)))) ;; Disable line numbers in some buffers
+
+;; Set theme
+(use-package nord-theme
+  :config (load-theme 'nord t))
+
+;; ------------------
+;; Behaviour
+;; ------------------
+(setq ring-bell-function 'ignore) ;; Disable annoying bell
+
 ;; Set tab width to 4
-(setq-default indent-tabs-mode nil)
+(setq-default indent-tabs-mode t)
 (setq-default tab-width 4)
 (setq c-default-style "linux")
 (setq c-basic-offset 4)
 (c-set-offset 'comment-intro 0)
 
+;; Set garbage collector to only collect every 50Mb
+;;(setq gc-cons-threshold 50000000)
+
 ;; Always follow symlinks
 (setq vc-follow-symlinks t)
+
+;; Don't freeze emacs with ctrl-z
+(global-unset-key (kbd "C-z"))
+
+;; Trim trailing whitespace when saving a file
+(add-hook 'before-save-hook
+          (lambda ()
+            (delete-trailing-whitespace)))
+
+;; Auto-reload changed buffers (eg. via git commands)
+;; Ensures the latest file is being edited
+(global-auto-revert-mode 1)
+(add-hook 'dired-mode-hook 'auto-revert-mode)
+
+;; Don't create backup files
+(setq backup-inhibited t)
+(setq auto-save-default nil)
 
 ;; set IDO mode
 (ido-mode 1)
 (setq ido-everywhere t)
-(setq ido-enable-flex-matching t)
+(use-package ido-completing-read+
+  :config (ido-ubiquitous-mode 1))
 
-;; KEY REMAPS
+;; tramp
+(setq tramp-auto-save-directory "/tmp")
+
+;; ------------------
+;; Key Remaps
+;; ------------------
 (global-set-key (kbd "M-o") 'other-window)
 (global-set-key (kbd "M-i") 'imenu)
 (global-set-key (kbd "C-v") ;; keep cursor centred when scrolling up
@@ -60,28 +119,17 @@
                   (scroll-down-command)
                   (recenter)))
 
-;; Don't freeze emacs with ctrl-z
-(global-unset-key (kbd "C-z"))
+;; ------------------
+;; Dired
+;; ------------------
+(require 'dired-x)
+(setq dired-listing-switches "-alh") ;; human-readable file sizes
+(setq dired-recursive-copies 'top)   ;; allow recursive copies (ask)
+(setq dired-recursive-deletes 'top)  ;; allow recursive deletes (ask)
 
-;; Don't create backup files
-(setq backup-inhibited t)
-(setq auto-save-default nil)
-
-;; view column number of cursor position
-(setq column-number-mode t)
-
-;; Line numbers
-(setq display-line-numbers-type 'relative)
-(global-display-line-numbers-mode)
-(dolist (mode '(org-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0)))) ;; Disable line numbers in some buffers
-
-;; Trim trailing whitespace when saving a file
-(add-hook 'before-save-hook
-          (lambda ()
-            (delete-trailing-whitespace)))
-
-;; tree-sitter
+;; ------------------
+;; Tree Sitter
+;; ------------------
 (setq treesit-language-source-alist
       '((c "https://github.com/tree-sitter/tree-sitter-c")
         (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
@@ -123,25 +171,52 @@
                             (setq-default c-ts-mode-indent-offset 4)
                             (c-ts-mode-set-global-style 'k&r)))
 
-;; Package management
-(require 'package)
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+;; ----------------------
+;; Package Configuration
+;; ----------------------
+;; Install the diminish package to not show the modes for each active package on the modeline
+;; Required before whitespace package configuration
+(use-package diminish)
 
-;; Initialize use-package
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(require 'use-package)
-(setq use-package-always-ensure t) ;; If not installed, install them automatically
-
-;; Editor theme
-(use-package the-matrix-theme
+;; For displaying a face where there is whitespace (tabs, spaces, trailing whitespace etc.)
+(use-package whitespace
+  :ensure
+  :defer 0.7
+  :diminish (global-whitespace-mode . "") ;; " $"
+  :custom
+  (whitespace-line-column 210) ;; limit line length
+  (whitespace-style
+   '(face tabs spaces space-mark tab-mark
+          trailing lines space-before-tab newline indentation empty space-after-tab))
+  (whitespace-global-modes '(prog-mode))
+  ;;:hook (prog-mode . whitespace-mode) ;; this caused problems in sql-mode (dunno why), global mode + list of modes to enable works better
   :config
-  (load-theme 'the-matrix t))
+  (global-whitespace-mode)
+  :custom-face
+  (whitespace-space ((t (:foreground "#3B4252" :background nil))))
+  (whitespace-indentation ((t (:foreground "#3B4252" :background nil))))
+  (whitespace-tab ((t (:foreground "#3B4252" :background nil))))
+  (whitespace-trailing ((t (:foreground "#EBCB8B" :background "#BF616A"))))
+  (whitespace-space-after-tab ((t (:foreground "#EBCB8B" :background "#D08770"))))
+  (whitespace-space-before-tab ((t (:foreground "#EBCB8B" :background "#D08770"))))
+  )
+
+;; ------------------
+;; External Packages
+;; ------------------
+;; The greatest git interface ever built
+(use-package magit
+  ;;:custom
+  ;;(magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1) ; Don't open new window when viewing diff
+  )
+
+;; Project management
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("C-c p" . projectile-command-map)))
 
 ;; Beacon to never lose the cursor again
 (use-package beacon
@@ -152,29 +227,19 @@
   (setq beacon-blink-delay 0.2)
   (beacon-mode 1))
 
-;; Project management
-(use-package projectile
-  :ensure t
-  :init
-  (projectile-mode +1)
-  :bind (:map projectile-mode-map
-              ("C-c p" . projectile-command-map)))
-
-;; Install the diminish package to not show the modes for each active package on the modeline
-(use-package diminish)
-
-;; For managing git repositories
-(use-package magit
-  ;;:custom
-  ;;(magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1) ; Don't open new window when viewing diff
-  )
-
 ;; Show available key-chords
 (use-package which-key
   :init (which-key-mode) ; init: runs before the package is loaded
   :diminish which-key-mode
   :config ; config: runs after the package is loaded
   (setq which-key-idle-delay 0.5))
+
+;; M-x enhancement built on top of IDO (M-x completion and autocomplete)
+(use-package smex
+  :config (smex-initialize)
+  :bind (("M-x" . smex)
+		 ("M-X" . smex-major-mode-commands)
+		 ("C-c C-c M-x" . execute-extended-command)))
 
 ;; Org-mode
 (use-package org)
